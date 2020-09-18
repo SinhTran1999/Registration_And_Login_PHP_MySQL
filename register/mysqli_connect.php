71fecd37906b5e44ec46bef5b679f64cbe4090ce
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-18 21:53:30 +0700

    create table user in database register_db on MySQL

--- register/mysqli_connect.php
+++ register/mysqli_connect.php
@@ -1,23 +1,30 @@
 <?php 
 
     // define constant variables
+    define ('DB_NAME', 'register_db');
     define ('DB_USER', 'root');
     define ('DB_PASSWORD', '');
     define ('DB_HOST', 'localhost');
 
     try{
         // connection variable
-        $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
-
-        // Create database
-        $sql = "CREATE DATABASE register_db";
-        if($con->query($sql) ===TRUE){
-            echo "Database created successfully";
+        $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
+        // Sql to create table
+        $sql = "CREATE TABLE user(
+            userID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstName varchar(100) NOT NULL,
+            lastName  varchar(100) NOT NULL,
+            email varchar(200) NOT NULL,
+            password varchar(200) NOT NULL,
+            profileImage varchar(255),
+            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )";
+        if($con->query($sql) == TRUE){
+            echo "Table register_db created successfully";
         }else{
-            echo "Error creating database: ".$con->error;
+            echo "Error creating table: " .$con->error;
         }
-        $con->close();
-
     }catch(Exception $ex){
         print "An Exception occurred Message". $ex->getMessage();
     }catch(Error $e){
