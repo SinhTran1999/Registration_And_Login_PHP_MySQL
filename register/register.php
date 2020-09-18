51fb2c03231d964a125804f775142a9abeab3fc2
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-18 14:14:25 +0700

    hander file register.php and style.css

--- register/register.php
+++ register/register.php
@@ -11,15 +11,55 @@
                         <p class="p-1 m-0 font-ubuntu text-black-50">Register and enjoy additional features </p>
                         <span class="font-ubuntu text-black-50"> I already have <a href="login.php">Login</a></span>
                     </div>
-                    <div class="upload-profile-image">
+                    <div class="upload-profile-image d-flex justify-content-center pb-5">
                         <div class="text-center">
                             <div class="d-flex justify-content-center">
                                 <img class="camera-icon" src="./assets/camera-solid.svg" alt="camera">
                             </div>
-                            <img src="./assets/profile/beard.png" class="img rounded-circle" alt="beard">
+                            <img src="./assets/profile/beard.png" style="width: 200px; height: 200px;" class="img rounded-circle" alt="beard">
                             <small class="form-text text-black-50">Choose Image</small>
                             <input type="file" class="form-control-file" name="profileUpload" id="upload-profile">
                         </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <form action="register.php" method="post" enctype="multipart/form-data" id="reg-form">
+                            <div class="form-row">
+                                <div class="col">
+                                    <input type="text" name="firstName" id="firstName" class='form-control' placeholder="First Name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" name="lastName" id="lastName" class='form-control' placeholder="Last Name">
+                                </div>
+                            </div>
+                            <div class="form-row my-4">
+                                <div class="col">
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
+                                </div>
+                            </div>
+
+                            <div class="form-row my-4">
+                                <div class="col">
+                                    <input type="password" name="password" id="password" class="form-control" placeholder="password" required>
+                                </div>
+                            </div>
+
+                            <div class="form-row my-4">
+                                <div class="col">
+                                    <input type="password" name="confirm_pwd" id="confirm_pwd" class="form-control" placeholder="Confirm Password" required>
+                                </div>
+                            </div>
+
+                            <div class="form-check form-check-inline">
+                                <input type="checkbox" name="argreement" class="form-check-input" required>
+                                <label for="agreement" class="form-check-label font-ubuntu text-black-50">
+                                    I agree <a href="#">term, conditions, and policy</a>(*)
+                                </label>
+                            </div>
+
+                            <div class="submit-btn text-center my-5">
+                                <button type="submit" class="btn btn-warning rounded-pill text-dark px-5">Continue</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
