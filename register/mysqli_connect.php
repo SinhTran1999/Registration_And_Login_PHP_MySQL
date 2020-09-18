56515b44fe1d2f55853426005481e94a34061621
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-18 20:48:14 +0700

    create database register_db on MySQL

--- register/mysqli_connect.php
+++ register/mysqli_connect.php
@@ -1,15 +1,26 @@
 <?php 
 
     // define constant variables
-    define ('DB_NAME', 'register_db');
     define ('DB_USER', 'root');
     define ('DB_PASSWORD', '');
     define ('DB_HOST', 'localhost');
 
     try{
         // connection variable
-        $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    }catch(Exception $ex){
+        $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
 
+        // Create database
+        $sql = "CREATE DATABASE register_db";
+        if($con->query($sql) ===TRUE){
+            echo "Database created successfully";
+        }else{
+            echo "Error creating database: ".$con->error;
+        }
+        $con->close();
+
+    }catch(Exception $ex){
+        print "An Exception occurred Message". $ex->getMessage();
+    }catch(Error $e){
+        print "The system is busy please try later";
     }
 ?>
